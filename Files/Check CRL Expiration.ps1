496cfe43d8d011bd9a16a21510c416c62dc79be7
yoavaha <127658771+yoavaha@users.noreply.github.com>
2024-04-07 17:08:54 +0300

    Add files via upload

---
+++ Files/Check CRL Expiration.ps1
@@ -0,0 +1,128 @@
+﻿# --------------------------------------------------------------------------------------------------------------------------
+# "CheckCRLExpiration" script was designed to check CRL files exposed by HTTP and validate their availability and
+# validity period based on a predefined threshold set by the user.
+# Written by Omer Eldan, Customer Engineer, Microsoft.
+# --------------------------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------------------------
+# Verify-CRLFile Function - Retrieve CRL information from a URL and verify expiration time based on a predefined threshold
+# --------------------------------------------------------------------------------------------------------------------------
+Function Verify-CRLFile ($CRLServer,$CRLFileName,$CRLExpirationThresholdInHours,$OutputDirectory,$SendAlertsByEmail,$SendAlertsToEventViewer) {
+    # Configure required variables for email notificaiton, if required.
+    If ($SendAlertsByEmail) {
+        $MailMessage = New-Object System.Net.Mail.MailMessage 
+        $MailMessage.From = "PKI@domain.local"  
+        $MailMessage.To.Add("Administator@domain.local") 
+        $MailMessage.IsBodyHtml = $True 
+        $MailMessage.Subject = "PKI - Check CRL Expiration Script"
+        $SMTPServer = "Exchange.domain.local" 
+        $SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer)  
+    }
+
+     # Check URL response status code to make sure the CRL file is available.
+    $CRLURLPath = "http://$CRLServer/CertEnroll/$CRLFileName"
+    $OutputFile = "$OutputDirectory\$CRLServer_$CRLFileName"
+    $WebRequestResponse = Invoke-WebRequest -Uri $CRLURLPath
+    If (($WebRequestResponse.StatusCode) -ne "200") {
+        $Message = "There might be a problem with CRL availability. The CRL file '$CRLFileName' on server '$CRLServer' is not accessible."
+        Write-Host $Message -ForegroundColor Red
+        if ($SendAlertsToEventViewer) {
+            Write-EventLog -LogName Application -Source "CRL Monitoring Script" -Message $Message -EntryType Error -EventId 1003
+        }
+        if ($SendAlertsByEmail) {
+            $MailMessage.Body = $Message 
+            $SMTPClient.Send($MailMessage)
+        }
+        Return   
+    }
+
+    # Check CRL expiration times and validate it against the predefined threshold.
+    Invoke-WebRequest -Uri $CRLURLPath -OutFile $OutputFile
+    $CRLData = Get-CertificateRevocationList $OutputFile
+    $TimeSpan = New-TimeSpan -End $CRLData.NextUpdate
+    if (($TimeSpan.Days*24+$TimeSpan.Hours) -le $CRLExpirationThresholdInHours) {
+        $Message = "There might be a problem with CRL updating process. The CRL file '$CRLFileName' on server '$CRLServer' will expire within $($TimeSpan.Days) days and $($TimeSpan.Hours) hours."
+        Write-Host $Message -ForegroundColor Red
+        if ($SendAlertsToEventViewer) {
+            Write-EventLog -LogName Application -Source "CRL Monitoring Script" -Message $Message -EntryType Warning -EventId 1002
+        }
+        if ($SendAlertsByEmail) {
+            $MailMessage.Body = $Message 
+            $SMTPClient.Send($MailMessage)
+        }
+    }
+    else {
+        $Message = "CRL file on server '$CRLServer' is updated, and is within the valid range of expiration ($CRLExpirationThresholdInHours hours). The CRL file '$CRLFileName' will expire within $($TimeSpan.Days) days and $($TimeSpan.Hours) hours."
+        Write-Host $Message -ForegroundColor Green
+        if ($SendAlertsToEventViewer) {
+            Write-EventLog -LogName Application -Source "CRL Monitoring Script" -Message $Message -EntryType Information -EventId 1001
+        }
+        if ($SendAlertsByEmail) {
+            $MailMessage.Body = $Message 
+            $SMTPClient.Send($MailMessage)
+        }
+    }
+}
+
+# --------------------------------------------------------------------------------------------------------------------------
+# Verify-CRLAvailabilityUsingNLB Function - Validate CRL availability when accessing the CRL using a NLB address
+# --------------------------------------------------------------------------------------------------------------------------
+Function Verify-CRLAvailabilityUsingNLB ($CRLNLBName,$CRLFileName,$SendAlertsByEmail,$SendAlertsToEventViewer) {
+    $CRLURLPath = "http://$CRLNLBName/CertEnroll/$CRLFileName"
+    $WebRequestResponse = Invoke-WebRequest -Uri $CRLURLPath
+    If (($WebRequestResponse.StatusCode) -eq "200") {
+        $Message = "CRL file named '$CRLFileName' is accessible and valid using the network load balancer address ($CRLURLPath)."
+        Write-EventLog -LogName Application -Source "CRL Monitoring Script" -Message $Message -EntryType Information -EventId 2001
+        Write-Host $Message -ForegroundColor Green   
+    }
+    else {
+        $Message = "There was a problem to access the CRL file named '$CRLFileName using the network load balancer address ($CRLURLPath)."
+        Write-EventLog -LogName Application -Source "CRL Monitoring Script" -Message $Message -EntryType Error -EventId 2002
+        Write-Host $Message -ForegroundColor Red
+    }
+}
+
+# --------------------------------------------------------------------------------------------------------------------------
+# Install and import required PowerShell modules
+# --------------------------------------------------------------------------------------------------------------------------
+<# Install PSPKI PowerShell module and its prerequisites for offline environments. Should be pefromed once:
+#  Download Nuget from an online, connected workstation, and copy the Nuget folder to "C:\Program Files\PackageManagement\ProviderAssemblies".
+#  Download PSPKI PowerShell module, and copy the whole folder to "C:\Program Files\WindowsPowerShell\Modules".
+#>
+
+Import-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208
+Import-Module PSPKI
+ 
+# --------------------------------------------------------------------------------------------------------------------------
+# Define general variables
+# --------------------------------------------------------------------------------------------------------------------------
+$SendAlertsByEmail = $false
+$SendAlertsToEventViewer = $true
+$OutputDirectory = "C:\Scripts\PKI Monitoring"
+$CRLServers = "CRL01.domain.local","CRL02.domain.local" ## Change accroding to your CRL server addresses
+$CRLNLBs = "CRLVIP01" ## Change accroding to your CRL network load balancer addresses
+
+# --------------------------------------------------------------------------------------------------------------------------
+# Validate prerequisites
+# --------------------------------------------------------------------------------------------------------------------------
+
+#Checks if the output folder is exist, and if not creates it.
+If ( -Not (Test-Path -Path $OutputDirectory) ) {
+    New-Item -ItemType Directory -Path $OutputDirectory
+}
+
+#Checks if Event Viewer is used by the script, and if so creates the event source if it's not already existed
+If ($SendAlertsToEventViewer) {
+    $EventLogName = "Application"
+    $EventSource = "CRL Monitoring Script"
+    if ([System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $false) {
+        [System.Diagnostics.EventLog]::CreateEventSource($EventSource,$EventLogName)
+    }    
+}
+
+# --------------------------------------------------------------------------------------------------------------------------
+# Main - Call relevant functions with provided parameters
+# --------------------------------------------------------------------------------------------------------------------------
+foreach ($CRLServer in $CRLServers) {
+    Verify-CRLFile -CRLServer $CRLServer -CRLFileName "SubCA01.CRL" -CRLExpirationThresholdInHours 48 -OutputDirectory $OutputDirectory -SendAlertsByEmail $false -SendAlertsToEventViewer $true
+}
---
+++ Files/Expiring Certificates Report.ps1
@@ -0,0 +1,143 @@
+﻿Function Create-HTMLReport ($CertificatesObjectsArray) {
+    $Date = Get-Date -Format "yyyy-MM-dd"
+    $ReportsDir = "C:\Reports"
+    If( -Not (Test-Path -Path $ReportsDir ) )
+    {
+        New-Item -ItemType Directory -Path $ReportsDir
+    }
+    $ReportFileName = "ADCSExpiringCerts_$Date.html"
+    $Body = "<H1>AD CS - Expiring Certificates Report</H1>
+    <br>
+    <table>
+        <tr>
+            <th>Request ID</th>
+            <th>Common Name</th>
+            <th>Expired On</th>
+            <th>Requester Name</th>
+            <th>Certificate Template</th>
+        </tr>"
+        foreach ($CertificateObject in $CertificatesObjectsArray) {
+            $Body += "<tr>
+                <td>$($CertificateObject.'Request ID')</td>
+                <td>$($CertificateObject.'Common Name')</td>
+                <td>$($CertificateObject.'Expired On')</td>
+                <td>$($CertificateObject.'Requester Name')</td>
+                <td>$($CertificateObject.'Certificate Template')</td>
+            </tr>"   
+        }
+    $ConvertParams = @{
+    head = @"
+    <Title>AD CS - Expiring Certificates Report</Title>
+    <Style>
+        body { 
+            background-color:#E5E4E2;
+            font-family:Arial, sans-serif;
+            font-size:10pt; }
+        table {
+            border-collapse: collapse;
+            width: 80%;
+            margin-left:auto; 
+            margin-right:auto; }
+        th, td {
+            padding: 12px;
+            text-align: left;
+            border-bottom: 1px solid #ddd; }
+        th {
+            color:white;
+            background-color:#525251; }
+        tr {background-color:#F0F0F0;}
+        tr:hover {background-color:#D4D4D4;}
+        H1 {
+            font-family:Tahoma;
+            color:#525251;
+            text-align:center; }
+        H@ {
+            font-family:Tahoma;
+            color:#6D7B8D; }
+    </style>
+"@
+ body = $Body
+}
+    ConvertTo-Html @ConvertParams | Out-File "$ReportsDir\$ReportFileName"
+    If(Test-Path -Path "$ReportsDir\$ReportFileName") {
+        Write-Host "The report has been created successfully in the following location: $ReportsDir\$ReportFileName" -ForegroundColor Green
+    }
+    Else {
+        Write-Host "Failed to create the report because of an error." -ForegroundColor Red
+    }
+}
+
+#Install-Module -Name PSPKI
+Import-Module -Name PSPKI
+
+#Initialize Parameters 
+$CertificateTemplates = @()
+$CertificateTemplatesOID = @()
+$CertificatesObjectsArray = @()
+$CertificateTemplatesFilter = "Web Server - SHA256","Contoso-CodeSigning","Contoso-SmartcardLogon" #Change to match organization certificate templates
+$CAName = "Contoso Subordinate" #Change to match organization CA Common Name
+$ExpirationTime = 72 #In Months
+
+#Select the Enterprise CA to review
+$EnterpriseCA = Get-CertificationAuthority -Name $CAName
+
+#Create an array of Certificate Templates objects based on the Certificate Template Filter configured in 'CertificateTemplatesFilter'
+foreach ($CertificateTemplateFilter in $CertificateTemplatesFilter) {
+    $CertificateTemplates += Get-CertificateTemplate -Name $CertificateTemplateFilter
+}
+
+#Create an array of Certificate Templates OIDs based on the Certificate Template objects array
+foreach ($CertificateTemplateObject in $CertificateTemplates) {
+    $CertificateTemplatesOID += $CertificateTemplateObject.OID.Value
+}
+
+#Configure a filter for certificates retrieval CmdLet (Get-IssuedRequest) 
+$IssuedRequestFilter = "NotAfter -ge $(Get-Date)", "NotAfter -le $((Get-Date).AddMonths($ExpirationTime))"
+
+#Get issued requests based on the filter created
+$ExpiringCertificates = Get-IssuedRequest -CertificationAuthority $EnterpriseCA -Filter $IssuedRequestFilter | Select-Object RequestId, Request.RequesterName, CommonName, NotAfter,CertificateTemplate
+
+#Filter retrieved certificates based on the their certificate template
+foreach ($Certificate in $ExpiringCertificates) {
+    if ($CertificateTemplatesOID.Contains($Certificate.CertificateTemplate)) {
+        $CertificateObject = New-Object -TypeName PSObject
+        $CertificateTemplate = Get-CertificateTemplate -OID $Certificate.CertificateTemplate
+        Add-Member -InputObject $CertificateObject -MemberType 'NoteProperty' -Name 'Request ID' -Value $($Certificate.RequestID)
+        Add-Member -InputObject $CertificateObject -MemberType 'NoteProperty' -Name 'Common Name' -Value $($Certificate.CommonName)
+        Add-Member -InputObject $CertificateObject -MemberType 'NoteProperty' -Name 'Expired On' -Value $($Certificate.NotAfter)
+        Add-Member -InputObject $CertificateObject -MemberType 'NoteProperty' -Name 'Requester Name' -Value $($Certificate.'Request.RequesterName')
+        Add-Member -InputObject $CertificateObject -MemberType 'NoteProperty' -Name 'Certificate Template' -Value $($CertificateTemplate.DisplayName)
+        $CertificatesObjectsArray += $CertificateObject
+    }
+}
+
+Create-HTMLReport($CertificatesObjectsArray)
+
+# ---------------------------------------------------------------------------------------------------------------
+# Send Mail - Define Variables
+# ----------------------------------------------------------------------------------------------------------------
+
+$date = (get-date -Format dd.MM.yyyy) 
+$fromaddress = "PKI@contoso.local" 
+$toaddress = "Administator@contoso.local" 
+$Subject = "PKI Expiring Certificates Report" 
+$Date = Get-Date -Format "yyyy-MM-dd"
+$ReportPath = "C:\Reports\ADCSExpiringCerts_$Date.html"
+$body = Get-Content $ReportPath
+#$attachment = "c:\Reports\Attachment.txt" 
+$smtpserver = "Exchange.contoso.local" 
+ 
+# ---------------------------------------------------------------------------------------------------------------
+# Send Mail - Create And Send The Mail
+# ----------------------------------------------------------------------------------------------------------------
+ 
+$message = New-Object System.Net.Mail.MailMessage 
+$message.From = $fromaddress 
+$message.To.Add($toaddress) 
+$message.IsBodyHtml = $True 
+$message.Subject = $Subject 
+#$attach = New-Object Net.Mail.Attachment($attachment) 
+#$message.Attachments.Add($attach) 
+$message.body = $body 
+$smtp = New-Object Net.Mail.SmtpClient($smtpserver) 
+$smtp.Send($message)
